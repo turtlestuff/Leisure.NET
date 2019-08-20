10b67df5b1ba452457daecb8dc7a4f880568e8d5
Vrabbers <nininininimocp@gmail.com>
2019-08-20 15:07:04 -0300

    Initial COMMIT

---
+++ Leisure.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leisure.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leisure.NET!");
+        }
+    }
+}
